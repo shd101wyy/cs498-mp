4db9e63070421a6691d12a87aec657d9c5422f4a
Yiyi Wang <shd101wyy@sina.com>
2015-09-10 21:42:49 -0500

    huhh

--- mp1/Assets/Scripts/Lightswitch.cs
+++ mp1/Assets/Scripts/Lightswitch.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LightSwitch : MonoBehaviour {
+public class Lightswitch : MonoBehaviour {
+	private bool pointLightOn;
+	private Light pointLight;
 
 	// Use this for initialization
 	void Start () {
-	
+		pointLightOn = true;
+		pointLight = GetComponent<Light> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			pointLightOn = !pointLightOn;
+			pointLight.enabled = pointLightOn;	
+		}
 	}
 }
