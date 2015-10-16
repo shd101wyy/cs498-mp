c49dd0ce5e70a8464d654946fa793a9d2a3e3910
Yiyi Wang <shd101wyy@sina.com>
2015-10-16 17:08:56 -0500

    66666

---
+++ final project/Assets/Scripts/GameStart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+
+	GameObject spaceStation;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Game Start");
+		spaceStation = GameObject.Find("SpaceStation");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spaceStation.transform.Rotate (0, 0, 0.5f);
+		spaceStation.transform.Translate (0.0001f, 0, 0.005f);
+	}
+}
