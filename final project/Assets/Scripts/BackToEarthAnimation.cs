c0e74c8153a25252e8bb23c77c76f918609623b3
Yiyi Wang <shd101wyy@sina.com>
2015-12-15 00:12:01 -0600

    ri le gou le

--- final project/Assets/Scripts/BackToEarthAnimation.cs
+++ final project/Assets/Scripts/BackToEarthAnimation.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BackToEarthAnimation : MonoBehaviour {
 
+	public Material farEarthSkybox;
+	public Material nearEarthSkybox;
 	public Material earthSkybox;
 	private GameObject cloud;
 	private GameObject particles;
@@ -14,19 +16,31 @@ public class BackToEarthAnimation : MonoBehaviour {
 		cloud.SetActive (false);
 	
 		particles = GameObject.Find ("particles");
+		particles.SetActive (false);
 	
 		cam = Camera.main;
+
+		RenderSettings.skybox = farEarthSkybox;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// this.transform.Rotate (Vector2.right, 2f, Space.Self);
-		particles.transform.eulerAngles = new Vector3(0, 90, 0); // paricle not rotate
+		particles.transform.eulerAngles = new Vector3(0, 0, 0); // paricle not rotate
 		particles.transform.position = cam.transform.position + new Vector3(0, -2, 0);
 	}
 
 	void enableCloud() {
 		cloud.SetActive (true);
+	}
+
+	void enableParticles() {
+		Debug.Log ("enable particles");
+		particles.SetActive (true);
+	}
+
+	void useNearEarthSkybox() {
+		RenderSettings.skybox = nearEarthSkybox;
 	}
 
 	void ChangeSkyBox() {
