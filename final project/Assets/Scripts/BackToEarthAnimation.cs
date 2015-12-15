296efb4e0846d6e38b3874e09fab7f9ba5e05705
Yiyi Wang <shd101wyy@sina.com>
2015-12-14 22:31:33 -0600

    add fire and particles

--- final project/Assets/Scripts/BackToEarthAnimation.cs
+++ final project/Assets/Scripts/BackToEarthAnimation.cs
@@ -5,16 +5,24 @@ public class BackToEarthAnimation : MonoBehaviour {
 
 	public Material earthSkybox;
 	private GameObject cloud;
+	private GameObject particles;
+	private Camera cam;
 
 	// Use this for initialization
 	void Start () {
 		cloud = GameObject.Find ("CloudsToy Mngr");
 		cloud.SetActive (false);
+	
+		particles = GameObject.Find ("particles");
+	
+		cam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// this.transform.Rotate (Vector2.right, 2f, Space.Self);
+		particles.transform.eulerAngles = new Vector3(0, 90, 0); // paricle not rotate
+		particles.transform.position = cam.transform.position + new Vector3(0, -2, 0);
 	}
 
 	void enableCloud() {
